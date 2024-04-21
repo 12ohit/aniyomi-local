a3b3f26e5b7f799bdbce4f4c7d2e4abfa2a7e7a1
12ohit <mrohit64@gmail.com>
2024-04-21 23:42:55 +0530

    catch all redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,10 @@ import { RouteName, RoutePath } from '../types/routes'
 const routes: RouteRecordRaw[] = [
   { path: RoutePath.HOME, redirect: { name: RouteName.ANIME } },
   { path: RoutePath.ANIME, name: RouteName.ANIME, component: AnimeLocalForm },
-  { path: RoutePath.MANGA, name: RouteName.MANGA, component: MangaLocalForm }
+  { path: RoutePath.MANGA, name: RouteName.MANGA, component: MangaLocalForm },
+  
+  // default redirect to home page
+  { path: '/:pathMatch(.*)*', redirect: RoutePath.HOME }
 ]
 
 export default routes
