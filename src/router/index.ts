9ea8e25014dc243a63349f8ea78b78684f113105
12ohit <mrohit64@gmail.com>
2024-04-21 23:36:03 +0530

    add routing support

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { RouteRecordRaw } from 'vue-router'
+import AnimeLocalForm from '../components/AnimeLocalForm.vue'
+import MangaLocalForm from '../components/MangaLocalForm.vue'
+import { RouteName, RoutePath } from '../types/routes'
+
+const routes: RouteRecordRaw[] = [
+  { path: RoutePath.HOME, redirect: { name: RouteName.ANIME } },
+  { path: RoutePath.ANIME, name: RouteName.ANIME, component: AnimeLocalForm },
+  { path: RoutePath.MANGA, name: RouteName.MANGA, component: MangaLocalForm }
+]
+
+export default routes
